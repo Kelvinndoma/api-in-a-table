eb259886208775c7710ed32d0e6a8bb592c3e2d5
kevin.mutua@student.moringaschool.com <kevin.mutua@student.moringaschool.com>
2022-12-21 23:08:55 +0300

    complete

--- index.js
+++ index.js
@@ -4,5 +4,16 @@ fetch('https://fakestoreapi.com/products')
 .then(data=>data.json())
 .then(data=>{
     console.log(data[0].title);
-
+    let tableData="";
+    data.map((values)=>{
+        tableData+=` <tr>
+        <td>${values.title}</td>
+      <td>${values.description}</td>
+      <td>${values.price}</td>
+      <td><img src="${values.image}"</td>
+    </tr>`;
+    });
+    document.getElementById("table-body").innerHTML=tableData;
+}).catch((err)=>{
+    console.log(err)
 })
